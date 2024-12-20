b62198c293bb1bb3962391d4303374e4740eb55a
2005LIKHITH <likhithcvsrl@gmail.com>
2024-12-20 20:46:33 +0530

    Prisma is Really Good

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello from index.ts");
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(username:string,password:string,age:number,city:string){
+    const res = await prisma.user.create({
+        data:{
+            username,
+            password,
+            age,
+            city
+        }
+    })
+}
+
+async function getUserDetails(username:string){
+    const res = await prisma.user.findMany({
+        where:{
+            username
+        }
+    })
+    console.log(res);
+    
+}
+
+getUserDetails("LikhithMar14")
